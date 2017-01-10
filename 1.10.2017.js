bae0537fb9a4b9d6ea4e1fb8cc136a352792fd00
Jack Linton <jacklinton@mac.com>
2017-01-10 14:36:19 -0500

    Finished last excesizes from the second part of this classwork on the original .js file.

--- 1.10.2017.js
+++ 1.10.2017.js
@@ -10,3 +10,15 @@ var interestList = ["napping", "computers", "horticulture", "taking walks", "pet
 debugger
 var myName = {first: "Jack", last: "Linton", dob: "10/24/1982", interests: interestList}
  console.log("Name:", myName.first, myName.last, "DOB:", myName.dob, "Interests are:", myName.interests[0],",", myName.interests[1], ",", myName.interests[2], ",", myName.interests[3], ",", myName.interests[4], ".")
+
+for(x=0;x<10;x++){
+  console.log(x)
+  // Comment out the below line
+  // to see what effect is has on output
+  //x = z + 10;
+  var y = x + 2;
+  // Comment out this line to only see
+  // the first log statement
+  console.log(y)
+  //return y
+}
