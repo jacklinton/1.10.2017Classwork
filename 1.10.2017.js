cedf8986b68746a5ecd53a0f75cd1403c1839d89
Jack Linton <jacklinton@mac.com>
2017-01-10 14:27:32 -0500

    Removed all unneccessary semicolons from javascript code.

--- 1.10.2017.js
+++ 1.10.2017.js
@@ -1,12 +1,12 @@
-var num1 = 3;
-var num2 = 4;
-console.log(num1 + num2);
+var num1 = 3
+var num2 = 4
+console.log(num1 + num2)
 
-var firstName = "Jack";
-var lastName = "Linton";
-alert("Full name " + firstName + " " + lastName + ".");
+var firstName = "Jack"
+var lastName = "Linton"
+alert("Full name " + firstName + " " + lastName + ".")
 
-var interestList = ["napping", "computers", "horticulture", "taking walks", "pets"];
-
-var myName = {first: "Jack", last: "Linton", dob: "10/24/1982", interests: interestList};
- console.log("Name:", myName.first, myName.last, "DOB:", myName.dob, "Interests are:", myName.interests[0],",", myName.interests[1], ",", myName.interests[2], ",", myName.interests[3], ",", myName.interests[4], ".");
+var interestList = ["napping", "computers", "horticulture", "taking walks", "pets"]
+debugger
+var myName = {first: "Jack", last: "Linton", dob: "10/24/1982", interests: interestList}
+ console.log("Name:", myName.first, myName.last, "DOB:", myName.dob, "Interests are:", myName.interests[0],",", myName.interests[1], ",", myName.interests[2], ",", myName.interests[3], ",", myName.interests[4], ".")
