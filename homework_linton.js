7a47db963dfcec4a53a93f748e0fd86f73716985
Jack Linton <jacklinton@mac.com>
2017-01-10 15:39:17 -0500

    Answered question number 3 on the homework and fixed the errors in the previous questions.

--- homework_linton.js
+++ homework_linton.js
@@ -8,10 +8,19 @@ var lastName = "Linton"
 console.log(firstName + lastName)//Defines two string variables, adds them together, and outputs the result to the console
 
 //Question 2
-var annuals = ["marigold", "petunia", "vinca"]
+var annuals = ["marigold", "petunia", "vinca"] //creates two individual arrays
 var perennials = ["allium", "aster", "lupin"]
-var garden = annuals + perennials //Creates a multidimensional array by adding two arrays
-log.console(garden[2][0]) //Outputs a value from each dimension of a multidimensional array
+var garden = [annuals, perennials] //Creates a multidimensional array by adding two arrays
+console.log(garden[1][0]) //Outputs a value from each dimension of a multidimensional array
+console.log(garden[0][0])
 
 //Question 3
- 
+var numberPrompt = prompt("Please enter a number", numberPrompt) //Asks the user to enter a number
+
+if (numberPrompt < 100) {										//Determines if that number is less than 100
+	alert("That number is less than 100!")						//Produces an output accordingly
+}
+else{
+	alert("Your number is " + numberPrompt + " and it is greater than 100!")
+}
+
