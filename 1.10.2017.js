100976553d30d8d0c7bb045cc401fcd90506037f
Jack Linton <jacklinton@mac.com>
2017-01-10 12:01:03 -0500

    added in some puntuation to the console output with personal info.

--- 1.10.2017.js
+++ 1.10.2017.js
@@ -9,4 +9,4 @@ alert("Full name " + firstName + " " + lastName + ".");
 var interestList = ["napping", "computers", "horticulture", "taking walks", "pets"];
 
 var myName = {first: "Jack", last: "Linton", dob: "10/24/1982", interests: interestList};
- console.log(myName.first, myName.last, myName.dob, "Interests are:", myName.interests[0],",", myName.interests[1], ",", myName.interests[2], ",", myName.interests[3], ",", myName.interests[4], ".");
+ console.log("Name:", myName.first, myName.last, "DOB:", myName.dob, "Interests are:", myName.interests[0],",", myName.interests[1], ",", myName.interests[2], ",", myName.interests[3], ",", myName.interests[4], ".");
