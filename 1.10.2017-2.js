63a1458a3be926466fea17c848430b2869f5b6cc
Jack Linton <jacklinton@mac.com>
2017-01-10 13:04:04 -0500

    Comleted the peanut butter and jelly sandwich algorithm excecise in hte comments section of the JavaScript document-2

---
+++ 1.10.2017-2.js
@@ -0,0 +1,32 @@
+// Get the jelly
+
+// Get the peanut butter
+
+// Get the bread
+
+// Spread peanut butter on one side and jelly on the other
+
+// Combine the two sides
+
+//Here's an example of an algorithm. What steps could we add before, after, or in between steps?
+
+/* Breaking it down: Where is the jelly? Get the jelly from x, and bring it to the
+table and place it on the table near your chair. Open The Jelly. Where is the peanut 
+butter? Get the peanut butter from x and bring it to the table and place it near the 
+chair on the table. Open the peanut butter. Where is the bread? Get the bread from x and 
+bring it to the table and place it on the table next to the jelly. Where are the plates? 
+Get a plate from x and bring it to the table and lay it in front of your chair. 
+Open the bread. Remove two slices from the top of the bag and place them flat and next to 
+each pther on the plate. Where is the knife? Get the knife from x and bring it to the 
+table and hold it in one hand. Use the knife to scoop some jelly from the jelly jar, then
+spread that jelly on one slice of bread on the side of the slice facing away from the
+plate. Make sure the entire face of that slice of bread has jelly on it. Use the knife
+to scoop some peanut butter out of the peanut butter jar and spread it on the other slice 
+of bread on the plate on the side of the sloce facing away from the plate. Lift the slice
+of bread with jelly on it with your left hand without touching the jelly. Lift the slice
+of bread with peanut butter on it with your right hand without touching the peanut
+butter. Place the side of the slice of bread in your left hand on top of the slice of bread 
+in your right such that the part of the slice with jelly is flat against the part of the
+other slice with peanut butter such that all the edges of the two slices coincide with 
+each other. Now the peanut butter and jelly sandwich is complete. */
+ 
