1c941f72b60ccbecdad5cdfa79ddbc78e1602538
Jack Linton <jacklinton@mac.com>
2017-01-10 15:10:23 -0500

    Started the .js file which will contain the homework and answered the first two questions.

---
+++ homework_linton.js
@@ -0,0 +1,17 @@
+//Question 1
+var num1 = 3
+var num2 = 4
+console.log(num1 + num2) //Defines two numerican variables, adds them together, and outputs them to the console
+
+var firstName = "Jack"
+var lastName = "Linton"
+console.log(firstName + lastName)//Defines two string variables, adds them together, and outputs the result to the console
+
+//Question 2
+var annuals = ["marigold", "petunia", "vinca"]
+var perennials = ["allium", "aster", "lupin"]
+var garden = annuals + perennials //Creates a multidimensional array by adding two arrays
+log.console(garden[2][0]) //Outputs a value from each dimension of a multidimensional array
+
+//Question 3
+ 
