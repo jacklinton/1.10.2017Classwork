154ef21dfdccc9338630d99ef334e802da5e5153
Jack Linton <jacklinton@mac.com>
2017-01-10 13:41:13 -0500

    Researched, found, and created a set of notes on how to move an object across the screen and how delay the action of a function by a set amout of time with links to references.

--- 1.10.2017-2.js
+++ 1.10.2017-2.js
@@ -45,3 +45,44 @@ each other. Now the peanut butter and jelly sandwich is complete. */
 the screen. Decide how long it will take for image to be completely off the screen. Devise 
 code that will accomplish the movement of the image in that direction in the chosen
 amount of time. */
+
+//Notes
+/* The two key methods to use with JavaScript are:
+
+setTimeout(function, milliseconds)
+Executes a function, after waiting a specified number of milliseconds.
+setInterval(function, milliseconds)
+Same as setTimeout(), but repeats the execution of the function continuously.
+http://www.w3schools.com/js/js_timing.asp 
+window.setTimeout(function, milliseconds);
+
+You might be interested in existing JavaScript based animation library: Script.Aculo.us
+
+A tutorial for moving apicture to the right: https://www.tutorialspoint.com/javascript/javascript_animation.htm
+
+From that page setTimeout( function, duration) − This function calls function after duration milliseconds from now.
+
+setInterval(function, duration) − This function calls function after every duration milliseconds.
+
+clearTimeout(setTimeout_variable) − This function calls clears any timer set by the setTimeout() functions.
+
+Animation Code
+JavaScript animations are done by programming gradual changes in an element's style.
+
+The changes are called by a timer. When the timer interval is small, the animation looks continuous.
+http://www.w3schools.com/js/js_htmldom_animate.asp
+
+*/
+//Notes
+
+
+/* Ok, so it looks like the general method of moving an object across the screen with 
+javascript is to create the item in the HTML code and give it an id. Then you create a 
+variable in your script which calls on the id using document.getElementById("id"). Then 
+you use the setTimeout(function, milliseconds) to say how long you want that object
+to move in milliseconds. Then create a function that tells the object to move from 
+position 0 (set by a variable) to another position or for a certain number of loop intervals
+within that function, or optionally you can use clearTimeout(funtion, interval) to stop the
+movment after a certain number of milliseconds. To delay the action of the move you
+use setTimeout(function, milliseconds) to delay the action of the movement by a number
+of milliseconds. Example can be found: http://www.w3schools.com/js/tryit.asp?filename=tryjs_dom_animate_3
