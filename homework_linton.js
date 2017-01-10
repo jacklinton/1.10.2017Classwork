2775ce062cf2d11209382574e15a82e1107ab3ac
Jack Linton <jacklinton@mac.com>
2017-01-10 16:20:38 -0500

    Finished up the homework problems and cleaned up the comments etc. No Longer returns undefined after running a function but returns 'Done.'

--- homework_linton.js
+++ homework_linton.js
@@ -28,10 +28,34 @@ else{
 var theName = prompt("What's your name?", theName) //Prompts user to enter their name
 
 function whatName(theName){						//Desclares a function whatName that can be called with the argument theName previously enterd by user
-	return console.log("Your name is", theName)//When function is called with theName the name entered is output to the cons ole
+	console.log("Your name is", theName)//When function is called with theName the name entered is output to the cons ole
+	return "Done."
 }
 
 //Question 7
 function uselessFunction(){						//Declares a function with no argument
-	return console.log("This function is nearly pointless.")//Shows an output when the function is called
+	console.log("This function is nearly pointless.")//Shows an output in the console when the function is called
+	return "Done."
+}
+
+//Question 8
+var theDoorNumber = prompt("Choose either door 1, 2, or 3 by entering the number now!", theDoorNumber)
+
+function behindTheDoor(theDoorNumber){		//The user is promted to enter a door number into variable 
+	if (theDoorNumber == 1) {				//theDoorNumber and when the function is called using 
+		console.log("Behind door number 1 is a car!")//theDoorNumber as an argument, various outputs to the console.
+		return "Done"
+	}
+	else if (theDoorNumber == 2) {
+		console.log("Behind door number 2 is a boat!")
+		return "Done."
+	}
+	else if (theDoorNumber == 3) {
+		console.log("Behind door number 3 is a party hat!")
+		return "Done."
+	}
+	else {
+		console.log("That's not one of the doors!")
+		return "Done."
+	}
 }
